d2d9b1514dd88ab288acba8aef7674478acc49a0
Douglas Orr <douglas.orr99@gmail.com>
2025-11-10 07:19:58 +0000

    Instanced grid view

--- src/core/view.ts
+++ src/core/view.ts
@@ -13,15 +13,106 @@ type Box = { left: number; right: number; bottom: number; top: number };
 class Logger {
   private trigger: boolean = false;
   constructor() {
-    window.addEventListener("click", (e) => {
+    window.addEventListener("click", () => {
       this.trigger = true;
     });
   }
   log(...args: any[]) {
     if (this.trigger) {
       console.log(...args);
-      this.trigger = false;
     }
+  }
+  tick() {
+    this.trigger = false;
+  }
+}
+const LOG = new Logger();
+
+class GridView {
+  private readonly mesh: THREE.InstancedMesh;
+  private readonly frameAttr: THREE.InstancedBufferAttribute;
+
+  constructor(private readonly game: G.Game, scene: THREE.Scene) {
+    const texture = new THREE.TextureLoader().load("img/cells.png");
+    const material = new THREE.ShaderMaterial({
+      uniforms: {
+        map: { value: texture },
+      },
+      vertexShader: `
+        attribute float tile;
+        varying float vTile;
+        varying vec2 vUv;
+        void main() {
+          vUv = uv;
+          vTile = tile;
+          gl_Position = projectionMatrix * viewMatrix * instanceMatrix * vec4(position, 1.0);
+        }
+      `,
+      fragmentShader: `
+        uniform sampler2D map;
+        varying vec2 vUv;
+        varying float vTile;
+        void main() {
+          vec2 uv = vUv;
+          uv.y = uv.y / 3.0 + vTile;
+          vec4 c = texture2D(map, uv);
+          if (c.a < 0.01) discard;
+          gl_FragColor = c;
+        }
+      `,
+      transparent: true,
+    });
+    this.mesh = new THREE.InstancedMesh(
+      new THREE.PlaneGeometry(1, 1),
+      material,
+      game.grid.cells.length
+    );
+    this.frameAttr = new THREE.InstancedBufferAttribute(
+      new Float32Array(this.mesh.count).fill(0),
+      /*itemSize*/ 1
+    );
+    this.mesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage);
+    this.mesh.geometry.setAttribute("tile", this.frameAttr);
+    scene.add(this.mesh);
+  }
+
+  // Update instance matrices to match layout.grid and the current game.grid
+  update(bounds: Box): void {
+    const grid = this.game.grid;
+    const cellSize = Math.min(
+      (bounds.right - bounds.left) / grid.cols,
+      (bounds.top - bounds.bottom) / grid.rows
+    );
+
+    const frameArr = this.frameAttr.array as Float32Array;
+    const mat = new THREE.Matrix4();
+    const pos = new THREE.Vector3();
+    const scale = new THREE.Vector3(cellSize, cellSize, 1);
+    for (let i = 0; i < grid.cells.length; i++) {
+      // Position
+      const row = Math.floor(i / grid.cols);
+      const col = i % grid.cols;
+      const x = bounds.left + (col + 0.5) * cellSize;
+      const y = bounds.bottom + (row + 0.5) * cellSize;
+      mat.compose(pos.set(x, y, 0), new THREE.Quaternion(), scale);
+      this.mesh.setMatrixAt(i, mat);
+
+      // Frame
+      const cell = grid.cells[i];
+      switch (cell) {
+        case G.Cell.O:
+          frameArr[i] = 2 / 3;
+          break;
+        case G.Cell.X:
+          frameArr[i] = 1 / 3;
+          break;
+        case G.Cell.W:
+          frameArr[i] = 0 / 3;
+          break;
+      }
+    }
+    this.mesh.instanceMatrix.needsUpdate = true;
+    this.frameAttr.needsUpdate = true;
   }
 }
 
@@ -30,13 +121,10 @@ class Renderer {
   private readonly scene: THREE.Scene;
   private readonly camera: THREE.OrthographicCamera;
   private lastTime: number | null = null;
-  private readonly logger: Logger = new Logger();
 
-  private gridBg: THREE.Mesh;
+  private readonly gridView: GridView;
 
-  constructor(game: G.Game, canvas: HTMLCanvasElement) {
-    void game; // TODO
-
+  constructor(private readonly game: G.Game, canvas: HTMLCanvasElement) {
     this.renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
     this.camera = new THREE.OrthographicCamera();
     this.camera.near = 0.1;
@@ -50,24 +138,8 @@ class Renderer {
     window.addEventListener("resize", this.onResize.bind(this));
     requestAnimationFrame(this.onAnimate.bind(this));
 
-    // Dummy content
-    const w = window.innerWidth;
-    const h = window.innerHeight;
-    const planeGeometry = new THREE.PlaneGeometry(w / 4, h / 4);
-    const texture = new THREE.TextureLoader().load("img/cells.png");
-    texture.magFilter = THREE.NearestFilter;
-    texture.minFilter = THREE.NearestFilter;
-    texture.wrapS = THREE.ClampToEdgeWrapping;
-    texture.wrapT = THREE.ClampToEdgeWrapping;
-    texture.offset.set(0, 0.5);
-    texture.repeat.set(1, 0.5);
-    const planeMaterial = new THREE.MeshBasicMaterial({
-      map: texture,
-      transparent: true,
-    });
-    this.gridBg = new THREE.Mesh(planeGeometry, planeMaterial);
-    this.gridBg.position.set(w / 2, h / 2, 0);
-    this.scene.add(this.gridBg);
+    // Views
+    this.gridView = new GridView(this.game, this.scene);
   }
 
   private onResize() {
@@ -83,31 +155,21 @@ class Renderer {
   }
 
   private onAnimate(time: number) {
+    // Preamble
     if (this.lastTime === null) {
       this.lastTime = time;
     }
-    // const dt = (time - this.lastTime) / 1000;
+    // const dt = (time - this.lastTime) / 1000; // TODO: animate
     this.lastTime = time;
-    // TODO - update animations
 
+    // Update views
     const layout = this.topLevelLayout();
-    this.logger.log(layout);
+    this.gridView.update(layout.grid);
 
-    this.gridBg.position.set(
-      (layout.grid.left + layout.grid.right) / 2,
-      (layout.grid.bottom + layout.grid.top) / 2,
-      0
-    );
-    this.gridBg.scale.set(
-      (layout.grid.right - layout.grid.left) /
-        this.gridBg.geometry.parameters.width,
-      (layout.grid.top - layout.grid.bottom) /
-        this.gridBg.geometry.parameters.height,
-      1
-    );
-
+    // Render
     this.renderer.render(this.scene, this.camera);
     requestAnimationFrame(this.onAnimate.bind(this));
+    LOG.tick();
   }
 
   private topLevelLayout(): { grid: Box; panel: Box } {
